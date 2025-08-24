50ae17e9002d863942aad5e9ff19c975aebb378b
Gravitas Industries <107733141+mavumo@users.noreply.github.com>
2025-08-24 19:00:29 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -77,11 +77,11 @@ wsApp.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
+server.listen({
     port,
     host: "0.0.0.0",
     reusePort: true,
-  }, () => {
+}, () => {
     log(`serving on port ${port}`);
-  });
+});
 })();
