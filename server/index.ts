560af2966b1fecf33616d194aff9476952bf840f
Gravitas Industries <107733141+mavumo@users.noreply.github.com>
2025-08-25 13:45:26 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -18,10 +18,12 @@ wsApp.use(express.urlencoded({ extended: false }));
   const server = await registerRoutes(wsApp);
 
   // Get the port from Render's environment variable
-  const port = process.env.PORT || 5000;
-
-  // Start the server
-  server.listen(port, () => {
-    log(`Server is live and listening on port ${port}`);
-  });
+ const port = parseInt(process.env.PORT, 10) || 5000;
+server.listen({
+    port,
+    host: "0.0.0.0",
+    reusePort: true,
+}, () => {
+    log(`serving on port ${port}`);
+});
 })();
